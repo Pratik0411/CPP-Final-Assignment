f7c7c014aa3d5f4fcbba1f9cdb27a9d48a260c1a
PratikPingate <98399182+PratikPingate@users.noreply.github.com>
2022-04-06 23:09:30 +0530

    Update Question3.cpp

--- Question3.cpp
+++ Question3.cpp
@@ -1,43 +1,63 @@
 #include<iostream>
 #include<string>
+#include<bits/stdc++.h>
+#include<algorithm>
+
 using namespace std;
 
-string swap(string b, int i1, int i2)
+string change(string b,int i1,int i2)
 {
-	char temp;
-	temp = b[i1];
-	b[i1] = b[i2];
-	b[i2] = temp;
+	string temp;
+	temp[0]=b[i1];
+	b[i1]=b[i2];
+	b[i2]=temp[0];
 	return b;
 }
 
-int calculate(string c)
+int calc(string c)
 {
 	int len;
-	int cnt = 0;
-	len = c.length();
-	for(int i=1; i < len; i++)
+	int cnt=0;
+	len=c.length();
+	for(int i=1;i<len;i++)
 	{
-		if(c[i-1] == '0' && c[i] == '1')
+		if(c[i-1]=='0' && c[i]=='1')
 		{
-			cnt++;
+			cnt=cnt+1;
 		}
 	}
 	return cnt;
 }
 
+int calculate(int a[],int len)
+{
+	int m=0;
+	for(int i=0;i<len;i++)
+	{
+		if(a[i]>m)
+		{
+			m = a[i];
+		}
+	}
+	return m;
+}
 int main()
 {
 	string b, c;
-	int i1, i2, count = 0;
-
+	int len, i1, i2, count = 0, cnt = 0;
 	cout << "Enter Binary String\n";
 	cin >> b;
-	
-	c = swap(b, 1, 3);
-	cout << c << endl;
-	count = calculate(c);
-	cout << count << endl;
-	
+	len = b.length();
+	int result[300];
+	for(int i=0;i<len;i++)
+	{
+		for(int j=i;j<len;j=j+2)
+		{
+			c = change(b, i, j);
+			result[cnt] = calc(c); 
+			cnt++;
+		}
+	}
+	cout<<"Answer: "<< calculate(result, cnt) << endl;
 	return 0;
 }
